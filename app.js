482fb97f6adac404ccb3b288f4c590e4f7acf5bb
ixyf <1551451231@qq.com>
2018-07-09 15:33:48 +0800

    提交了啊

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录2333333332啊啊啊
+    // 登录 我在这里改了代码
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
