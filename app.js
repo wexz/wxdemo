69c2f70bdad40890c6da385e4254b53be9b260d3
ixyf <1551451231@qq.com>
2018-07-09 15:29:21 +0800

    asd

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录233333333
+    // 登录2333333332
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
